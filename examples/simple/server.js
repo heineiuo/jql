20df6ca50b40519e468952beb603b0322e54b605
heineiuo <heineiuo@gmail.com>
2017-08-22 09:50:15 +0800

    example: update

--- examples/simple/server.js
+++ examples/simple/server.js
@@ -3,19 +3,21 @@ import morgan from 'morgan'
 import bodyParser from 'body-parser'
 import { expressJQL } from '../../packages/server/src'
 
+const { middleware } = expressJQL({
+  actions: {
+    getTime: () => (dispatch, getState) => new Promise(resolve => {
+      resolve(new Date())
+    }) 
+  }
+})
+
 const app = express()
 
 app.use(morgan('dev'))
 app.use(bodyParser.json())
 
 
-app.use('/jql', expressJQL({
-  actions: {
-    getTime: () => (dispatch, getState) => new Promise(resolve => {
-      resolve(new Date())
-    }) 
-  }
-}))
+app.use('/jql', middleware)
 
 app.use(express.static(__dirname))
 
